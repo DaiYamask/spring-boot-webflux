f8e51f621bacd412590f4526d73b541722841716
Dai Yamasaki <dai@tagbangers.co.jp>
2019-09-18 17:22:37 +0900

    code smells...

--- src/main/java/com/example/hellowebflux/HelloController.java
+++ src/main/java/com/example/hellowebflux/HelloController.java
@@ -26,6 +26,26 @@ public class HelloController {
 			}
 		}
 
+		for (int i = 0; i < 1; i++) {
+			for (int j = 0; j < 1; j++) {
+				for (int k = 0; k < 1; k++) {
+					for (int l = 0; l < 1; l++) {
+						System.out.println(l);
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < 1; i++) {
+			for (int j = 0; j < 1; j++) {
+				for (int k = 0; k < 1; k++) {
+					for (int l = 0; l < 1; l++) {
+						System.out.println(l);
+					}
+				}
+			}
+		}
+
 		return Flux.just("hoge", "fugo");
 	}
 }
